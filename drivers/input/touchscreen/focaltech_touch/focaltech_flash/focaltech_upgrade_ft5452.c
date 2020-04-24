63d6d687fc86195f683d395c26f6260cc9535cab
Andy Chang <andychangzxx@outlook.com>
2020-04-24 10:06:20 +0800

    input: Import xiaomi's touchscreen drivers
    Xiaomi use touch_irq_boost() in touchscreen drivers,
    this require changes in cpu-boost.c, so I remove it
    because I don't want it.
    
    Signed-off-by: Andy Chang <andychangzxx@outlook.com>

--- drivers/input/touchscreen/focaltech_touch/focaltech_flash/focaltech_upgrade_ft5452.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_flash/focaltech_upgrade_ft5452.c
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: GPL-2.0-only
 /*
  *
  * FocalTech fts TouchScreen driver.
  *
- * Copyright (c) 2012-2019, Focaltech Ltd. All rights reserved.
+ * Copyright (c) 2010-2018, Focaltech Ltd. All rights reserved.
+ * Copyright (C) 2020 XiaoMi, Inc.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -32,6 +32,7 @@
 /*****************************************************************************
 * 1.Included header files
 *****************************************************************************/
+#include "../focaltech_core.h"
 #include "../focaltech_flash.h"
 
 /************************************************************************
@@ -41,7 +42,7 @@
 * Output:
 * Return: return 0 if success, otherwise return error code
 ***********************************************************************/
-static int fts_ft5452_upgrade(u8 *buf, u32 len)
+static int fts_ft5452_upgrade(struct i2c_client *client, u8 *buf, u32 len)
 {
 	int ret = 0;
 	u32 start_addr = 0;
@@ -64,7 +65,7 @@ static int fts_ft5452_upgrade(u8 *buf, u32 len)
 	}
 
 	/* enter into upgrade environment */
-	ret = fts_fwupg_enter_into_boot();
+	ret = fts_fwupg_enter_into_boot(client);
 	if (ret < 0) {
 		FTS_ERROR("enter into pramboot/bootloader fail,ret=%d", ret);
 		goto fw_reset;
@@ -72,7 +73,7 @@ static int fts_ft5452_upgrade(u8 *buf, u32 len)
 
 	cmd[0] = FTS_CMD_FLASH_MODE;
 	cmd[1] = FLASH_MODE_UPGRADE_VALUE;
-	ret = fts_write(cmd, 2);
+	ret = fts_i2c_write(client, cmd, 2);
 	if (ret < 0) {
 		FTS_ERROR("upgrade mode(09) cmd write fail");
 		goto fw_reset;
@@ -82,13 +83,13 @@ static int fts_ft5452_upgrade(u8 *buf, u32 len)
 	cmd[1] = BYTE_OFF_16(len);
 	cmd[2] = BYTE_OFF_8(len);
 	cmd[3] = BYTE_OFF_0(len);
-	ret = fts_write(cmd, FTS_CMD_DATA_LEN_LEN);
+	ret = fts_i2c_write(client, cmd, FTS_CMD_DATA_LEN_LEN);
 	if (ret < 0) {
 		FTS_ERROR("data len cmd write fail");
 		goto fw_reset;
 	}
 
-	ret = fts_fwupg_erase(FTS_REASE_APP_DELAY);
+	ret = fts_fwupg_erase(client, FTS_REASE_APP_DELAY);
 	if (ret < 0) {
 		FTS_ERROR("erase cmd write fail");
 		goto fw_reset;
@@ -96,17 +97,17 @@ static int fts_ft5452_upgrade(u8 *buf, u32 len)
 
 	/* write app */
 	start_addr = upgrade_func_ft5452.appoff;
-	ecc_in_host = fts_flash_write_buf(start_addr, buf, len, 1);
-	if (ecc_in_host < 0 ) {
+	ecc_in_host = fts_flash_write_buf(client, start_addr, buf, len, 1);
+	if (ecc_in_host < 0) {
 		FTS_ERROR("lcd initial code write fail");
 		goto fw_reset;
 	}
 
-	FTS_INFO( "**********read out checksum**********");
+	FTS_INFO("**********read out checksum**********");
 
 	/* check sum init */
 	wbuf[0] = FTS_CMD_ECC_INIT;
-	ret = fts_write(wbuf, 1);
+	ret = fts_i2c_write(client, wbuf, 1);
 	if (ret < 0) {
 		FTS_ERROR("ecc init cmd write fail");
 		return ret;
@@ -123,7 +124,7 @@ static int fts_ft5452_upgrade(u8 *buf, u32 len)
 	wbuf[6] = BYTE_OFF_0(len);
 
 	FTS_DEBUG("ecc calc startaddr:0x%04x, len:%d", start_addr, len);
-	ret = fts_write(wbuf, 7);
+	ret = fts_i2c_write(client, wbuf, 7);
 	if (ret < 0) {
 		FTS_ERROR("ecc calc cmd write fail");
 		return ret;
@@ -135,7 +136,7 @@ static int fts_ft5452_upgrade(u8 *buf, u32 len)
 	for (i = 0; i < FTS_RETRIES_ECC_CAL; i++) {
 		wbuf[0] = FTS_CMD_FLASH_STATUS;
 		reg_val[0] = reg_val[1] = 0x00;
-		fts_read(wbuf, 1, reg_val, 2);
+		fts_i2c_read(client, wbuf, 1, reg_val, 2);
 		FTS_DEBUG("[UPGRADE]: reg_val[0]=%02x reg_val[0]=%02x!!", reg_val[0], reg_val[1]);
 		if ((0xF0 == reg_val[0]) && (0x55 == reg_val[1])) {
 			break;
@@ -145,9 +146,9 @@ static int fts_ft5452_upgrade(u8 *buf, u32 len)
 
 	/* read out check sum */
 	wbuf[0] = FTS_CMD_ECC_READ;
-	ret = fts_read(wbuf, 1, reg_val, 1);
+	ret = fts_i2c_read(client, wbuf, 1, reg_val, 1);
 	if (ret < 0) {
-		FTS_ERROR( "ecc read cmd write fail");
+		FTS_ERROR("ecc read cmd write fail");
 		return ret;
 	}
 	ecc_in_tp = reg_val[0];
@@ -159,7 +160,7 @@ static int fts_ft5452_upgrade(u8 *buf, u32 len)
 	}
 
 	FTS_INFO("upgrade success, reset to normal boot");
-	ret = fts_fwupg_reset_in_boot();
+	ret = fts_fwupg_reset_in_boot(client);
 	if (ret < 0) {
 		FTS_ERROR("reset to normal boot fail");
 	}
@@ -169,7 +170,7 @@ static int fts_ft5452_upgrade(u8 *buf, u32 len)
 
 fw_reset:
 	FTS_INFO("upgrade fail, reset to normal boot");
-	ret = fts_fwupg_reset_in_boot();
+	ret = fts_fwupg_reset_in_boot(client);
 	if (ret < 0) {
 		FTS_ERROR("reset to normal boot fail");
 	}
